30b158640cc2d5decefe514d3f49175539cf9eb3
leoarmelin <leonardoarmelin@codeby.com.br>
2021-07-24 12:40:20 -0300

    feat(typography): create system

---
+++ styles/typography.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const TitleThreeRegular = styled.Text`
+  font-family: 'ubuntu-regular';
+  font-size: 20px;
+`;
+
+export const Body = styled.Text`
+  font-family: 'ubuntu-regular';
+  font-size: 16px;
+`;
